2dc1418f472d9e7cf5b05cba753336f754f6f275
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2018-10-14 16:46:16 +0530

    Created seed file.

---
+++ db/seed.rb
@@ -0,0 +1,7 @@
+Joke.find_or_create_by!(body: "Knock! Knock! Whos there? Owls say. Owls say who? Yes, they do.")
+
+Joke.find_or_create_by!(body: "Q. What is the biggest lie in the entire universe? A. “I have read and agree to the Terms & Conditions.”")
+
+Joke.find_or_create_by!(body: "I just got fired from my job at the keyboard factory. They told me I wasn’t putting in enough shifts.")
+
+Joke.find_or_create_by!(body: "“I feel like carp today” “Yeah, you look a little fishy”")
